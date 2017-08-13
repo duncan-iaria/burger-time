4e75cb70c1fc0db8775609b503230f7ee8872c97
Snoodleking <duncaniaria@gmail.com>
2017-08-12 23:05:45 -0400

    added handlebars and setup basic view/partial

--- server/server.js
+++ server/server.js
@@ -1,12 +1,31 @@
 require( 'dotenv' ).config( { path: './server/.env' } );
 const express = require( 'express' );
+const exphbs  = require( 'express-handlebars' );
 
-//server
+//=========================
+//  SERVER
+//=========================
 const server = express();
-
-//server vars
 const PORT = process.env.PORT;
 const router = express.Router();
+
+//=========================
+//  VIEW ENGINE
+//=========================
+server.engine( 'handlebars', exphbs( { defaultLayout: 'main' } ) );
+server.set( 'view engine', 'handlebars' );
+
+//=========================
+//  MIDDLEWARE
+//=========================
+server.use( express.static( "public" ) );
+server.use( bodyParser.urlencoded( { extended: false } ) );
+
+//basic test route
+server.get( '/', function ( tRequest, tResponse )
+{
+    tResponse.render( 'index' );
+});
 
 //=========================
 //  INIT
@@ -15,5 +34,5 @@ server.listen( PORT, onServerInit )
 
 function onServerInit()
 {
-    console.log( `Server is listening at ${PORT}` );
+    console.log( `Server is listening at ${ PORT }` );
 }
