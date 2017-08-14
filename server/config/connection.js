207bba353c16edd38d7746181f5ee71aa07560f8
SNOODBOOK\MiniSnood <duncaniaria@gmail.com>
2017-08-14 19:07:25 -0400

    added support for JAWSDB (mysql hosting through heroku)

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,24 @@
 const mysql = require( 'mysql2' );
+let connection;
 
-const connection = mysql.createConnection
-(
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    }
-);
+//use JAWSDB if it's detected/otherwise use local
+if( process.env.JAWSDB_URL )
+{
+    connection = mysql.createConnection( process.env.JAWSDB_URL );
+}
+else
+{
+    connection = mysql.createConnection
+    (
+        {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        }
+    );
+}
 
 //=========================
 //  CONNECTION LOGIC
